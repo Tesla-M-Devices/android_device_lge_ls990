3324d3f9f2ebaf12b8e520c040250f12233227b0
Matthew Opperman <mattopp17@gmail.com>
2016-03-22 01:29:49 -0400

    clang_03, krait_tunings, and enable_gcconly true

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,11 +29,11 @@ TARGET_KERNEL_CONFIG := cyanogenmod_ls990_defconfig
 TARGET_REQUIRES_BUMP := true
 
 # Optimizations
-CLANG_O3 := false
+CLANG_O3 := true
 STRICT_ALIASING := false
-KRAIT_TUNINGS := false
+KRAIT_TUNINGS := true
 GRAPHITE_OPTS := false
-ENABLE_GCCONLY := false
+ENABLE_GCCONLY := true
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
